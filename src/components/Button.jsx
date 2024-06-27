9b68a5db5fd44d625a6022ed2797ff87c3c85928
unknown <urielcw54@gmail.com>
2024-06-26 21:04:55 -0300

    Finish

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,17 @@
 import '../css/Button.css';
 
-const Button = ({ type, className, children, onClick }) => (
-  <button type={type} className={`button ${className}`} onClick={onClick}>
-    {children}
-  </button>
-);
+const Button = ({ type, className, children, onClick }) => {
+  const handleClick = (event) => {
+    if (window.confirm('¿Estás seguro de que deseas continuar?')) {
+      onClick(event);
+    }
+  };
+
+  return (
+    <button type={type} className={`button ${className}`} onClick={handleClick}>
+      {children}
+    </button>
+  );
+};
 
 export default Button;
